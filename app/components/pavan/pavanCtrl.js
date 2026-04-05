e52238f05b6defc649845a2652f7f665f8ed8a6b
Vaibhav Rakheja <vaibhavrakheja11@gmail.com>
2026-04-04 23:37:17 -0400

    react deploemnt

--- app/components/pavan/pavanCtrl.js
+++
@@ -1,4 +0,0 @@
-angular.module('myAppPavanCtrl', []).controller('pavanCtrl', ['$scope', 'pavanContent', '$window', '$sce', function($scope, pavanContent, $window, $sce){
-    $window.scrollTo(0, 0);
-    $scope.pavanVideoIds = ['NcKUwYlBdxs', 'rQiyZuMRYdA', 'LwVUdcShfzI', 'v0KCW1CtqF4'];
-}]);
